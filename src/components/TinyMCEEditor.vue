46a58f96e8dfba376f4f5b1729fa7df51595d611
sreangsoeu <sreangsoeu767@gmail.com>
2025-06-05 15:55:54 +0700

    wgwe

--- src/components/TinyMCEEditor.vue
+++ src/components/TinyMCEEditor.vue
@@ -8,6 +8,7 @@ import { onMounted, onBeforeUnmount } from 'vue';
 onMounted(() => {
   tinymce.init({
     selector: '#editor',
+    apiKey: 'ajzwlify6vbdufx0b0egj9mbea4cq1sh2ds59xzrde9mryly',
     plugins: 'mathType chemType',
     toolbar: 'undo redo | bold italic | mathType chemType',
     readonly: false,
