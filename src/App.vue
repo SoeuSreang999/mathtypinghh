9c0cb36a0096137caa2e25603bc8f87e022f57b3
sreangsoeu <sreangsoeu767@gmail.com>
2025-06-05 14:37:52 +0700

    add

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <TinyMCEEditor />
+</template>
+
+<script setup lang="ts">
+import TinyMCEEditor from './components/TinyMCEEditor.vue';
+</script>
---
+++ src/components/TinyMCEEditor.vue
@@ -0,0 +1,20 @@
+<template>
+  <textarea id="editor"></textarea>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount } from 'vue';
+
+onMounted(() => {
+  tinymce.init({
+    selector: '#editor',
+    plugins: 'mathType chemType',
+    toolbar: 'undo redo | bold italic | mathType chemType',
+    readonly: false,
+  });
+});
+
+onBeforeUnmount(() => {
+  tinymce.remove('#editor');
+});
+</script>
